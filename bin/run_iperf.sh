50d819c8021aafd695299cf0c7ba05f299763791
dlawogud000 <jh020826@gmail.com>
2026-03-31 17:01:04 +0900

    a

--- bin/run_iperf.sh
+++ bin/run_iperf.sh
@@ -25,11 +25,11 @@ if [ "$DIRECTION" = "downlink" ]; then
 fi
 
 if [ "$PROTOCOL" = "tcp" ]; then
-  iperf3 -B "$LOCAL_IP -c "$SERVER_IP" $REVERSE_FLAG -t "$DURATION" -i 1 --json \
+  iperf3 -B "$LOCAL_IP" -c "$SERVER_IP" $REVERSE_FLAG -t "$DURATION" -i 1 --json \
     > "$OUT_DIR/iperf.json" 2> "$OUT_DIR/iperf.stderr.log"
 elif [ "$PROTOCOL" = "udp" ]; then
   UDP_RATE="${UDP_RATE:-100M}"
-  iperf3 -c "$SERVER_IP" -u $REVERSE_FLAG -b "$UDP_RATE" -t "$DURATION" -i 1 --json \
+  iperf3 -B "$LOCAL_IP" -c "$SERVER_IP" -u $REVERSE_FLAG -b "$UDP_RATE" -t "$DURATION" -i 1 --json \
     > "$OUT_DIR/iperf.json" 2> "$OUT_DIR/iperf.stderr.log"
 else
   echo "[ERROR] Unsupported protocol: $PROTOCOL"
